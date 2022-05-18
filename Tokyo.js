ae31a06bbbeb04918553b98fefe252b1d27446ce
pitabogdan2002 <bogdan-ioan.pita@s.unibuc.ro>
2022-05-18 21:15:27 +0300

    Commit 5

--- Tokyo.js
+++ Tokyo.js
@@ -1,13 +1,27 @@
 window.onload = init;
 function init()
 {
-    const paragrafe= document.getElementsByClassName("paragraf");
-    paragrafe.forEach(element => {
-        element.addEventListener("click",function(event)
-        {
-            
-            if(element.id == "copil")
-                event.stopPropagation();
-        })
-    });
+    const paragrafe = document.getElementsByClassName("paragraf");
+    paragrafe[0].addEventListener("click",parinte);
+    paragrafe[1].addEventListener("click",copil);
+    paragrafe[2].addEventListener("click",nepot);
+}
+function copil(event) 
+{
+    var element = event.currentTarget;
+    element.style.color = "blue";
+    event.stopPropagation();
+}
+
+function parinte(event) 
+{
+ 
+    var element = event.currentTarget;
+    element.style.color = "aqua";
+}
+function nepot(event) 
+{
+    var element = event.currentTarget;
+    element.style.color = "purple";
+    event.stopPropagation();
 }
